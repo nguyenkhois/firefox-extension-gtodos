fb56aa70965eebf7e45f91f6f500872bee8ea54d
Khoi Le <minarek@outlook.com>
2018-08-25 09:31:59 +0200

    Updated UI

--- src/menu.js
+++ src/menu.js
@@ -11,15 +11,12 @@ const mapStateToProps = (state) => {
 class MenuClass extends Component {
     render() {
         const TopMenuHome = () => (
-            <div className="top-menu-home">
+            <div className="top-menu">
                 <table>
                     <tbody>
                         <tr>
                             <td>
                                 <Link to={'/deleted'} name={'Deleted tasks'}/>
-                            </td>
-                            <td>
-                                <svg aria-hidden="true" data-prefix="fas" data-icon="angle-right" className="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
                             </td>
                         </tr>
                     </tbody>
@@ -28,12 +25,12 @@ class MenuClass extends Component {
         )
 
         const TopMenuDeletedTasks = () => (
-            <div className="top-menu-left">
+            <div className="top-menu">
                 <table>
                     <tbody>
                         <tr>
                             <td>
-                                <svg aria-hidden="true" data-prefix="fas" data-icon="angle-left" className="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 20.5"><path fill="currentColor" d="M9.537,20.083.292,10.946a.975.975,0,0,1,0-1.392L9.537.417a1.456,1.456,0,0,1,2.041,0,1.415,1.415,0,0,1,0,2.016L3.669,10.25l7.909,7.815a1.417,1.417,0,0,1,0,2.017,1.456,1.456,0,0,1-2.041,0"></path></svg>
                             </td>
                             <td>
                                 <Link to={'/'} name={'Home'}/>
